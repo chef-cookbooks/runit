11158d2d1807a97063e95346e4c5b604285ce40b
Brian Akins <brian.akins@turner.com>
2012-05-03 21:32:24 -0400

    use default, not set

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,13 +19,13 @@
 
 case platform
 when "ubuntu","debian"
-  set[:runit][:sv_bin] = "/usr/bin/sv"
-  set[:runit][:chpst_bin] = "/usr/bin/chpst"
-  set[:runit][:service_dir] = "/etc/service"
-  set[:runit][:sv_dir] = "/etc/sv"
+  default[:runit][:sv_bin] = "/usr/bin/sv"
+  default[:runit][:chpst_bin] = "/usr/bin/chpst"
+  default[:runit][:service_dir] = "/etc/service"
+  default[:runit][:sv_dir] = "/etc/sv"
 when "gentoo"
-  set[:runit][:sv_bin] = "/usr/bin/sv"
-  set[:runit][:chpst_bin] = "/usr/bin/chpst"
-  set[:runit][:service_dir] = "/etc/service"
-  set[:runit][:sv_dir] = "/var/service"
+  default[:runit][:sv_bin] = "/usr/bin/sv"
+  default[:runit][:chpst_bin] = "/usr/bin/chpst"
+  default[:runit][:service_dir] = "/etc/service"
+  default[:runit][:sv_dir] = "/var/service"
 end
