c3d46bcf2c330e90aff8f1bb1e1701d57078cca8
jtimberman <joshua@chef.io>
2015-03-25 12:19:46 -0600

    Add support for Fedora 21, kitchen updates
    This commit mainly adds support for Fedora 21, which now has packages in
    imeyer's Package Cloud repository.
    
    - Update centos base boxes to latest releases
    - Add ubuntu 14.04
    - Add fedora to 'rhel' conditionals where applicable
    - Register the 'runit_service' resource with MiniTest::Chef so it can be
      asserted using the runit state instead of underlying system service
      state checking because that generally will attempt to use the "lsb
      init interface," which doesn't work with systemd

--- attributes/default.rb
+++ attributes/default.rb
@@ -36,7 +36,7 @@ when 'debian'
     default['runit']['reload'] = 'reload runsvdir'
   end
 
-when 'rhel'
+when 'rhel', 'fedora'
   default['runit']['sv_bin'] = '/sbin/sv'
   default['runit']['chpst_bin'] = '/sbin/chpst'
   default['runit']['service_dir'] = '/etc/service'
