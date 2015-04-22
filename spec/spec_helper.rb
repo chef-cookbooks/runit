c87b7984d4a167031bddbd96168abd8e8bf2da11
Cameron Johnston <cameron@rootdown.net>
2015-04-21 20:05:42 -0600

    modify spec_helper to support cookbook resolvers other than Berkshelf

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,19 @@
+COOKBOOK_RESOLVERS = {
+  'batali' => ['Batali', 'batali/chefspec'],
+  'berkshelf' => ['Berksfile', 'chefspec/berkshelf'],
+  'librarian' => ['Cheffile', 'chefspec/librarian']
+}
+
 require 'chefspec'
-require 'chefspec/berkshelf'
+
+if(ENV['COOKBOOK_RESOLVER'])
+  require COOKBOOK_RESOLVERS[ENV['COOKBOOK_RESOLVER']]
+else
+  resolver_lib = COOKBOOK_RESOLVERS.values.detect do |r_file, r_lib|
+    File.exists?(File.join(File.dirname(__FILE__), '..', r_file))
+  end
+  raise "Failed to locate valid cookbook resolver files!" unless resolver_lib
+  require resolver_lib.last
+end
 
 at_exit { ChefSpec::Coverage.report! }
