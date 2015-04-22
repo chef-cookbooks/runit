161f15a55ed2e13347042304c90f7aceb7e42f9c
Cameron Johnston <cameron@rootdown.net>
2015-04-21 21:03:13 -0600

    adjust spec_helper to satisfy rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,13 +6,13 @@ COOKBOOK_RESOLVERS = {
 
 require 'chefspec'
 
-if(ENV['COOKBOOK_RESOLVER'])
+if ENV['COOKBOOK_RESOLVER']
   require COOKBOOK_RESOLVERS[ENV['COOKBOOK_RESOLVER']]
 else
-  resolver_lib = COOKBOOK_RESOLVERS.values.detect do |r_file, r_lib|
-    File.exists?(File.join(File.dirname(__FILE__), '..', r_file))
+  resolver_lib = COOKBOOK_RESOLVERS.values.detect do |r_file, _r_lib|
+    File.exist?(File.join(File.dirname(__FILE__), '..', r_file))
   end
-  raise "Failed to locate valid cookbook resolver files!" unless resolver_lib
+  fail 'Failed to locate valid cookbook resolver files!' unless resolver_lib
   puts "Resolving cookbooks from #{resolver_lib.first}"
   require resolver_lib.last
 end
