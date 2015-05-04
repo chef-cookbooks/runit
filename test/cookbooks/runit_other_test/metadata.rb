8280835ef7451d16aa35ef89deb13ee1b801230c
Cameron Johnston <cameron@rootdown.net>
2015-05-04 14:50:11 -0600

    Add test coverage for template cookbook option
    Adding tests to exercise runit_service 'cookbook' parameter. See #114.

---
+++ test/cookbooks/runit_other_test/metadata.rb
@@ -0,0 +1,6 @@
+name 'runit_other_test'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'This cookbook is used with test-kitchen to test the "template cookbook" option in the runit cookbook'
+version '1.0.0'
