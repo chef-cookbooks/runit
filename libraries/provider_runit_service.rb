482910f56b06d98dd99f2ba692a5d65b33bd22aa
Cameron Johnston <cameron@rootdown.net>
2015-06-23 12:44:43 -0600

    enable action should wait for service "ok" socket

--- libraries/provider_runit_service.rb
+++ libraries/provider_runit_service.rb
@@ -243,10 +243,15 @@ class Chef
           action :create
         end
 
-        # FIXME: replace me
-        # ruby_block 'wait_for_service' do
-        #   block wait_for_service
-        # end
+        ruby_block 'wait_for_service' do
+          block do
+            sleep 1 until ::FileTest.pipe?("#{service_dir_name}/supervise/ok")
+            if new_resource.log
+              sleep 1 until ::FileTest.pipe?("#{service_dir_name}/log/supervise/ok")
+            end
+          end
+          action :run
+        end
       end
 
       # signals
