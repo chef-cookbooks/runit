fc6c5afd75914bb0a8aef45f8037fc505878b953
Cameron Johnston <cameron@rootdown.net>
2015-12-17 16:52:16 -0700

    bump version to 1.7.7 for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@hw-ops.com'
 license 'Apache 2.0'
 description 'Installs runit and provides runit_service definition'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.7.6'
+version '1.7.7'
 
 recipe 'runit', 'Installs and configures runit'
 
