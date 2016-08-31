d4f353384713698269abef921c37972b0819b092
Tim Smith <tsmith@chef.io>
2016-08-30 17:21:55 -0700

    Run specs against the latest RHEL 5
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/runit_service/centos_5_service_spec.rb
+++ spec/runit_service/centos_5_service_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe 'runit_test::service on centos-5.8' do
-  cached(:centos_58_service) do
+  cached(:centos_5_service) do
     ChefSpec::SoloRunner.new(
       platform: 'centos',
-      version: '5.8',
+      version: '5.11',
     # step_into: 'runit_service'
     ) do |node|
       node.normal['runit']['version'] = '0.0'
@@ -14,67 +14,67 @@ describe 'runit_test::service on centos-5.8' do
   # Resource in runit_test::service
   context 'compiling the test recipe' do
     it 'creates runit_service[plain-defaults]' do
-      expect(centos_58_service).to enable_runit_service('plain-defaults')
+      expect(centos_5_service).to enable_runit_service('plain-defaults')
     end
 
     it 'creates runit_service[no-svlog]' do
-      expect(centos_58_service).to enable_runit_service('no-svlog')
+      expect(centos_5_service).to enable_runit_service('no-svlog')
     end
 
     it 'creates runit_service[default-svlog]' do
-      expect(centos_58_service).to enable_runit_service('default-svlog')
+      expect(centos_5_service).to enable_runit_service('default-svlog')
     end
 
     it 'creates runit_service[checker]' do
-      expect(centos_58_service).to enable_runit_service('checker')
+      expect(centos_5_service).to enable_runit_service('checker')
     end
 
     it 'creates runit_service[finisher]' do
-      expect(centos_58_service).to enable_runit_service('finisher')
+      expect(centos_5_service).to enable_runit_service('finisher')
     end
 
     it 'creates runit_service[env-files]' do
-      expect(centos_58_service).to enable_runit_service('env-files')
+      expect(centos_5_service).to enable_runit_service('env-files')
     end
 
     it 'creates runit_service[template-options]' do
-      expect(centos_58_service).to enable_runit_service('template-options')
+      expect(centos_5_service).to enable_runit_service('template-options')
     end
 
     it 'creates runit_service[control-signals]' do
-      expect(centos_58_service).to enable_runit_service('control-signals')
+      expect(centos_5_service).to enable_runit_service('control-signals')
     end
 
     it 'creates runit_service[runsvdir-floyd]' do
-      expect(centos_58_service).to enable_runit_service('runsvdir-floyd')
+      expect(centos_5_service).to enable_runit_service('runsvdir-floyd')
     end
 
     it 'creates runit_service[timer]' do
-      expect(centos_58_service).to enable_runit_service('timer')
+      expect(centos_5_service).to enable_runit_service('timer')
     end
 
     it 'creates runit_service[chatterbox]' do
-      expect(centos_58_service).to enable_runit_service('chatterbox')
+      expect(centos_5_service).to enable_runit_service('chatterbox')
     end
 
     it 'creates runit_service[floyds-app]' do
-      expect(centos_58_service).to enable_runit_service('floyds-app')
+      expect(centos_5_service).to enable_runit_service('floyds-app')
     end
 
     it 'creates runit_service[yerba]' do
-      expect(centos_58_service).to enable_runit_service('yerba')
+      expect(centos_5_service).to enable_runit_service('yerba')
     end
 
     it 'creates runit_service[yerba-alt]' do
-      expect(centos_58_service).to enable_runit_service('yerba-alt')
+      expect(centos_5_service).to enable_runit_service('yerba-alt')
     end
 
     it 'creates runit_service[ayahuasca]' do
-      expect(centos_58_service).to enable_runit_service('ayahuasca')
+      expect(centos_5_service).to enable_runit_service('ayahuasca')
     end
 
     it 'creates runit_service[exist-disabled]' do
-      expect(centos_58_service).to disable_runit_service('exist-disabled')
+      expect(centos_5_service).to disable_runit_service('exist-disabled')
     end
   end
 end
