f49414066463798125d2d1edbc4f0530c87d6212
Tim Smith <tsmith@chef.io>
2016-12-05 12:19:51 -0800

    Remove deprecated manage_home directive
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/runit_test/recipes/default.rb
+++ test/cookbooks/runit_test/recipes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: runit_test
+# Cookbook:: runit_test
 # Recipe:: default
 #
-# Copyright 2012-2016, Chef Software, Inc.
+# Copyright:: 2012-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- test/cookbooks/runit_test/recipes/service.rb
+++ test/cookbooks/runit_test/recipes/service.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: runit_test
+# Cookbook:: runit_test
 # Recipe:: service
 #
-# Copyright 2012-2016, Chef Software, Inc.
+# Copyright:: 2012-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -40,7 +40,6 @@ user 'floyd' do
   shell '/bin/bash'
   home '/home/floyd'
   manage_home true
-  supports manage_home: true
 end
 
 %w(sv service).each do |dir|
