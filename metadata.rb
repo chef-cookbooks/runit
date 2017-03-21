c3a9ba6068e44330e9298453daaf0fdeb34082b3
Tim Smith <tsmith@chef.io>
2017-03-21 16:03:49 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'runit'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs runit and provides runit_service resource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.0.5'
--- test/cookbooks/runit_test/metadata.rb
+++ test/cookbooks/runit_test/metadata.rb
@@ -1,7 +1,7 @@
 name 'runit_test'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'This cookbook is used with test-kitchen to test the parent, runit cookbook'
 version '1.0.1'
 
