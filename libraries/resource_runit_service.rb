355105baef01f4cb315094b9d5f3a7fd708dab03
Balasankar "Balu" C <balasankar@gitlab.com>
2019-02-25 23:42:38 +0530

    Make runit service's control commands configurable
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- libraries/resource_runit_service.rb
+++ libraries/resource_runit_service.rb
@@ -58,9 +58,12 @@ class Chef
       property :finish_script_template_name, String, default: lazy { service_name }
       property :control_template_names, Hash, default: lazy { set_control_template_names }
       property :status_command, String, default: lazy { "#{sv_bin} status #{service_name}" }
+      property :start_command, String, default: 'start'
+      property :stop_command, String, default: 'stop'
+      property :restart_command, String, default: 'restart'
       property :sv_templates, [true, false], default: true
       property :sv_timeout, Integer
-      property :sv_verbose, [true, false], default: false
+      property :sv_verbose, [TrueClass, FalseClass], default: false
       property :log_dir, String, default: lazy { ::File.join('/var/log/', service_name) }
       property :log_flags, String, default: '-tt'
       property :log_size, Integer
@@ -100,10 +103,10 @@ class Chef
           find_resource(:service, new_resource.name) do # creates if it does not exist
             provider Chef::Provider::Service::Simple
             supports new_resource.supports
-            start_command "#{new_resource.sv_bin} start #{service_dir_name}"
-            stop_command "#{new_resource.sv_bin} stop #{service_dir_name}"
-            restart_command "#{new_resource.sv_bin} restart #{service_dir_name}"
             status_command new_resource.status_command
+            start_command "#{new_resource.sv_bin} #{new_resource.start_command} #{service_dir_name}"
+            stop_command "#{new_resource.sv_bin} #{new_resource.stop_command} #{service_dir_name}"
+            restart_command "#{new_resource.sv_bin} #{new_resource.restart_command} #{service_dir_name}"
             action :nothing
           end
         end
