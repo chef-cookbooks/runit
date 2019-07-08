45e9abb153868da70dfc4e0dd3b34e9f6861006f
Tim Smith <tsmith84@gmail.com>
2019-07-08 11:18:06 -0700

    Disable FC009 in the package resource
    Chef 15 moved response_file out of the package resource so disabling
    this here is now necessary
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -43,9 +43,9 @@ when 'debian'
                'runit'
              end
 
-  package pkg_name do
+  package pkg_name do # ~FC009
     action :install
-    response_file 'runit.seed'
+    response_file 'runit.seed' 
   end
 else
   raise 'The cookbook only supports Debian/RHEL based Linux distributions. If you believe further platform support is possible please open a pull request.'
