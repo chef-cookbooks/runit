cc5582db7cb816d28d8e7b1af15371205c205fb7
Tim Smith <tsmith84@gmail.com>
2019-10-30 14:11:34 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs runit and provides runit_service resource'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '5.1.2'
-
-recipe 'runit', 'Installs and configures runit'
 
 %w(ubuntu debian centos redhat amazon scientific oracle enterpriseenterprise zlinux).each do |os|
   supports os
