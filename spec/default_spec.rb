0b8ecea344bce6da682625f25877f667f7d05d82
Tim Smith <tsmith84@gmail.com>
2020-02-26 14:11:43 -0800

    Remove Ubuntu 14.04 specs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -85,19 +85,6 @@ describe 'runit::default' do
     end
   end
 
-  context 'on Ubuntu 14.04' do
-    platform 'ubuntu', '14.04'
-
-    it 'installs the runit package' do
-      is_expected.to install_package('runit')
-    end
-
-    it 'starts and enabled the correct runit service' do
-      is_expected.to enable_service('runsvdir')
-      is_expected.to start_service('runsvdir')
-    end
-  end
-
   context 'on Ubuntu 16.04' do
     platform 'ubuntu', '16.04'
 
